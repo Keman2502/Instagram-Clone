66069663b7e9110d8b7c25ea10366e8834a05d3b
Keman2502 <86626003+Keman2502@users.noreply.github.com>
2021-08-10 15:09:19 +0500

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,19 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
+  
+  
+  config.action_mailer.default_url_options = { :host => 'instagram-1010.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'instagram-1010.herokuapp.com',
+    user_name: 'keman2502@gmail.com',
+    password: 'ipwhhsstkxfyfboe',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
